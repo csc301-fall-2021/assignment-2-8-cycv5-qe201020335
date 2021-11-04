1fcdffe6f76502f7fdddbb7194e7d5203b47326e
Yichen Cai <charlestsai0919@gmail.com>
2021-11-04 13:12:22 -0400

    configure testing;

--- test/test_pytest.py
+++ test/test_pytest.py
@@ -7,13 +7,12 @@ import pytest
 BASE = "http://127.0.0.1:5000/"
 
 
-# @pytest.fixture
-# def app():
-#     flask_app.run(debug=True)
+@pytest.fixture
+def app():
+    subprocess.Popen(["python", "main.py"])
 
 
 def test_upload():
-    subprocess.Popen(["python", "main.py"])
     file = {'file': open("test/time_series_test.csv")}
     response = requests.post(BASE + "time_series/death", files=file)
     assert response.content == b'Success'
