7337bbfcbfaa45b3ebedb3e5b423aa75bd4de773
Yichen Cai <charlestsai0919@gmail.com>
2021-11-04 01:33:19 -0400

    bug fix

--- test/test_pytest.py
+++ test/test_pytest.py
@@ -3,13 +3,9 @@ import pytest
 
 BASE = "http://127.0.0.1:5000/"
 
-@pytest.mark.upload
+
 def test_upload():
-    file = {'file': open("time_series_test.csv")}
+    file = {'file': open("test/time_series_test.csv")}
     response = requests.post(BASE + "time_series/death", files=file)
     assert response.content == b'Success'
     file['file'].close()
-
-@pytest.mark.fail
-def test_fail():
-    assert 1 == 2
