ae7a6a0e9d4861ba9e2c8a7a65c379489d3694b3
Yichen Cai <charlestsai0919@gmail.com>
2021-11-04 13:33:26 -0400

    introduce delay

--- test/test_pytest.py
+++ test/test_pytest.py
@@ -15,11 +15,11 @@ server = subprocess.Popen(["python", "main.py"])
 
 
 def test_upload():
-    sleep(3)
+    sleep(2)
     file = {'file': open("test/time_series_test.csv")}
     response = requests.post(BASE + "time_series/death", files=file)
     assert response.content == b'Success'
     file['file'].close()
 
 
-# server.terminate()
+server.terminate()
