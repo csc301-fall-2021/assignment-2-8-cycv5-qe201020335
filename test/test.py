7220b62a46e74e0cad86c81e54d5bef83466d99e
Yichen Cai <charlestsai0919@gmail.com>
2021-11-04 01:09:43 -0400

    implemented first test;

--- test/test.py
+++ test/test.py
@@ -7,13 +7,10 @@ BASE = "http://127.0.0.1:5000/"
 class TestWebApi(unittest.TestCase):
 
     def test_upload(self):
-        file = {'file': open("test/time_series_test.csv", 'rb')}
-        response = requests.post(BASE + "time_series/", files=file)
-        assert response.json() == {'first line in the file': "b'Province/State,Country/Region,Lat,Long,1/22/20,1/23/20,1/24/20,1/25/20,1/26/20\\r\\n'"}
-
-    def test_get(self):
-        response = requests.get(BASE + "query/china/death")
-        assert response.json() == {'info': 'death', 'place': 'china'}
+        file = {'file': open("time_series_test.csv")}
+        response = requests.post(BASE + "time_series/death", files=file)
+        assert response.content == b'Success'
+        file['file'].close()
 
 
 if __name__ == '__main__':
