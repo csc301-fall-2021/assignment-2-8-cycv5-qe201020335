2ec7136268e6144844b08225705bc9f1bf8c5daf
Yichen <charlestsai0919@gmail.com>
2021-11-04 20:11:16 -0400

    Update test suite;

--- test/test_pytest.py
+++ test/test_pytest.py
@@ -8,15 +8,21 @@ import pytest
 BASE = "http://127.0.0.1:5000/"
 
 server = subprocess.Popen(["python", "main.py"])
-
+sleep(2)
 # @pytest.fixture
 # def test_app():
 #     subprocess.Popen(["python", "main.py"])
 
 
-def test_upload():
-    sleep(2)
+def test_upload_time_series():
     file = {'file': open("test/time_series_test.csv")}
     response = requests.post(BASE + "time_series/death", files=file)
+    assert response.content == b'Success'
+    file['file'].close()
+
+
+def test_upload_daily_report():
+    file = {'file': open("test/daily_report_test.csv")}
+    response = requests.post(BASE + "daily_reports", files=file)
     assert response.content == b'Success'
     file['file'].close()
